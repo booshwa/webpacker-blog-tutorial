974e2821543604ba0f1a4e01bcdb681acc2c37af
Bernard Conrad <bernard.j.conrad@gmail.com>
2019-01-03 15:45:02 -0600

    Finished React Article

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Timestamp from 'react-timestamp'
+
 class Article extends React.Component {
   render () {
     return (
@@ -11,8 +13,9 @@ class Article extends React.Component {
           {this.props.description}
           <div className='article-meta-details'>
             <small>
-              Created by: {this.props.author},{this.props.created_at} ago, last
-              updated: {this.props.updated_at} ago
+              Created by: {this.props.author},{' '}, &nbsp;
+              <Timestamp time={this.props.created_at} precision={3} />, 
+              last updated: <Timestamp time={this.props.updated_at} precision={3} />
             </small>
           </div>
         </div>
@@ -23,10 +26,10 @@ class Article extends React.Component {
 
 Article.propTypes = {
   title: PropTypes.string,
-  path:  PropTypes.string,
+  path: PropTypes.string,
   description: PropTypes.string,
   author: PropTypes.string,
   created_at: PropTypes.string,
   updated_at: PropTypes.string
-};
+}
 export default Article
